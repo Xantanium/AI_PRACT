fbf4e7bbd3bdd841959e539af2ba57367a9ead41
Xantanium <xantiumplaques@gmail.com>
2025-05-02 08:08:48 +0530

    Initial Commit

---
+++ build/CMakeFiles/BFS_DFS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BFS_DFS"
+  "BFS_DFS.pdb"
+  "CMakeFiles/BFS_DFS.dir/BFS_DFS.cpp.o"
+  "CMakeFiles/BFS_DFS.dir/BFS_DFS.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BFS_DFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
