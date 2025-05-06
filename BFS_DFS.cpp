e45dc54d8bc1bcf24ad025ea14787879ebdc7f43
Xantanium <xantiumplaques@gmail.com>
2025-05-06 18:02:12 +0530

    Final BFS DFS

--- BFS_DFS.cpp
+++ BFS_DFS.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 class Graph {
-    int m_v;
+    int v;
     vector<vector<int>> adj;
 
 public:
@@ -18,27 +18,29 @@ public:
     void dfs(int n);
 
 private:
-    void dfsUtil(int n, vector<bool> visited);
+    void dfsUtil(int n, vector<bool>& visited);
 };
 
 int main(int argc, char* argv[])
 {
     Graph g(5);
 
-    g.addEdge(1, 5);
-    g.addEdge(1, 5);
-    g.addEdge(1, 5);
-    g.addEdge(1, 5);
-    g.addEdge(1, 5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
 
-    g.bfs(1);
-    g.dfs(1);
+    cout << "BFS from node 0: ";
+    g.bfs(0);
+
+    cout << "\nDFS from node 0: ";
+    g.dfs(0);
 
     return 0;
 }
 
 Graph::Graph(int v)
-    : m_v(v)
+    : v(v)
 {
     this->adj.resize(v);
 }
@@ -49,25 +51,39 @@ void Graph::addEdge(int u, int v)
     adj[v].push_back(u);
 }
 
-void Graph::bfs(int n)
+void Graph::bfs(int start)
 {
-    vector<bool> visited(m_v, false);
+    vector<bool> visited(v, false);
     queue<int> q;
+    q.push(start);
+    visited[start] = true;
 
-    visited[n] = true;
-    q.push(n);
-
-    cout << "Starting bfs from node: " << n << endl;
     while (!q.empty()) {
         int node = q.front();
         q.pop();
         cout << node << " ";
 
-        for (auto neighbour : adj[node]) {
-            if (!visited[neighbour]) {
-                visited[neighbour] = true;
-                q.push(neighbour);
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
             }
         }
+    }
+}
+
+void Graph::dfs(int start)
+{
+    vector<bool> visited(v, 0);
+    dfsUtil(start, visited);
+}
+
+void Graph::dfsUtil(int n, vector<bool>& visited)
+{
+    visited[n] = true;
+    cout << n << " ";
+    for (int neighbour : adj[n]) {
+        if (!visited[neighbour])
+            dfsUtil(neighbour, visited);
     }
 }
